5d6f53df2ecaafefb8c7aec7f0c3da01c92d7b0c
Roland Boon <roland.boon@live.nl>
2026-07-19 19:20:57 +0200

    Getting EnumBooleanConverter working on WPF and UNO. Tested on Windows.

--- dotNetFractal.Uno/ConvertBitmapToImageSource.cs
+++ dotNetFractal.Uno/ConvertBitmapToImageSource.cs
@@ -1,5 +1,9 @@
-using Microsoft.UI.Xaml.Media.Imaging;
 using SkiaSharp;
+#if __IOS__ || __MACCATALYST__ || __ANDROID__ || __WASM__
+using Microsoft.UI.Xaml.Media.Imaging;
+#else // Windows/Desktop
+using SkiaSharp.Views.Windows;
+#endif
 
 namespace dotNetFractal.Uno;
 
@@ -12,15 +16,7 @@ public static class ConvertBitmapToImageSource
     /// <returns>The image as a BitmapImage for UNO</returns>
     public static ImageSource Clone(SKBitmap src)
     {
-        using var image = SKImage.FromBitmap(src);
-        using var data = image.Encode(SKEncodedImageFormat.Png, 100);
-        using var ms = new MemoryStream();
-        data.SaveTo(ms);
-        ms.Seek(0, SeekOrigin.Begin);
-
-        var bitmapImage = new BitmapImage();
-        bitmapImage.SetSource(ms.AsRandomAccessStream());
-        return bitmapImage;
+        return ConvertFast(src);
     }
 
     /// <summary>
@@ -31,9 +27,10 @@ public static class ConvertBitmapToImageSource
     /// <returns>The image as a WriteableBitmap for UNO</returns>
     public static ImageSource ConvertFast(SKBitmap src)
     {
+#if __IOS__ || __MACCATALYST__ || __ANDROID__ || __WASM__
         var width = src.Width;
         var height = src.Height;
-
+        
         var writeableBitmap = new WriteableBitmap(width, height);
 
         using var image = SKImage.FromBitmap(src);
@@ -44,6 +41,9 @@ public static class ConvertBitmapToImageSource
         writeableBitmap.SetSource(ms.AsRandomAccessStream());
 
         return writeableBitmap;
+#else // Windows/Desktop
+        return src.ToWriteableBitmap(); // Ensure the bitmap is in a format that can be used with WriteableBitmap
+#endif
     }
 }
 
--- dotNetFractal.WPF/EnumBooleanConverter.cs
+++ dotNetFractal.WPF/EnumBooleanConverter.cs
@@ -1,25 +1,82 @@
-using System;
 using System.Globalization;
+using System.Reflection;
+using System.Windows;
 using System.Windows.Data;
 
 namespace dotNetFractal.WPF;
 
 public class EnumBooleanConverter : IValueConverter
 {
+    private Type? _lastEnumType;
+
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        return value.Equals(parameter);
+        if (parameter is string parameterString && value != null)
+        {
+            // Cache the enum type for use in ConvertBack
+            _lastEnumType = value.GetType();
+
+            return Enum.IsDefined(value.GetType(), value) && 
+                   Enum.Parse(value.GetType(), parameterString).Equals(value);
+        }
+        return value?.Equals(parameter) ?? false;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        ArgumentNullException.ThrowIfNull(value, nameof(value));
         ArgumentNullException.ThrowIfNull(parameter, nameof(parameter));
-        ArgumentNullException.ThrowIfNull(targetType, nameof(targetType));
-        if (!targetType.IsInstanceOfType(parameter))
+
+        // Only update when RadioButton is checked (true), not when unchecked (false)
+        if (value is bool boolValue && !boolValue)
         {
-            throw new ArgumentException($"Parameter must be of type {targetType}", nameof(parameter));
+            return DependencyProperty.UnsetValue;
         }
-        return (bool)value ? parameter : default!;
+
+        if (parameter is string parameterString)
+        {
+            // Try to use the cached enum type from the last Convert call
+            Type? enumType = _lastEnumType;
+
+            // If we don't have a cached type, try to find it by searching for an enum with this value
+            if (enumType == null || !enumType.IsEnum)
+            {
+                enumType = FindEnumTypeByValue(parameterString);
+            }
+
+            if (enumType != null && enumType.IsEnum)
+            {
+                return Enum.Parse(enumType, parameterString);
+            }
+        }
+
+        if (targetType.IsEnum)
+        {
+            return Enum.Parse(targetType, parameter.ToString()!);
+        }
+
+        return parameter;
+    }
+
+    private Type? FindEnumTypeByValue(string valueName)
+    {
+        // Search in the current app's assembly and dotNetFractal.WPF assembly
+        var assemblies = new[]
+        {
+            Assembly.GetExecutingAssembly(),
+            typeof(ResolutionEnum).Assembly
+        };
+
+        foreach (var assembly in assemblies.Distinct())
+        {
+            var enumType = assembly.GetTypes()
+                .FirstOrDefault(t => t.IsEnum && Enum.GetNames(t).Contains(valueName));
+
+            if (enumType != null)
+            {
+                return enumType;
+            }
+        }
+
+        return null;
     }
 }
